08d478afdc92b5759e00392adec833f3a704c63d
Fabrício Girardi Andreis <00028478@randon.com.br>
2021-03-02 18:28:56 -0300

    Removing TEF enum

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,6 @@ export enum Name {
   TedSameHolderRefundCredit = 'TED_SAME_HOLDER_REFUND_CREDIT', // Estorno TED mesma titularidade
   TedOtherHolderRefundCredit = 'TED_OTHER_HOLDER_REFUND_CREDIT', // Estorno TED outra titularidade
 
-  // TEF: Transferência Eletrônica Financeira
-  TefSameHolderDebit = 'TEF_SAME_HOLDER_DEBIT', // Envio TEF mesma titularidade
-  TefOtherHolderDebit = 'TEF_OTHER_HOLDER_DEBIT', // Envio TEF outra titularidade
-  TefSameHolderCredit = 'TEF_SAME_HOLDER_CREDIT', // Recebimento TEF mesma titularidade
-  TefOtherHolderCredit = 'TEF_OTHER_HOLDER_CREDIT', // Recebimento TEF outra titularidade
-
   // DOC: Documento de Ordem de Crédito
   DocSameHolderDebit = 'DOC_SAME_HOLDER_DEBIT', // Envio DOC mesma titularidade
   DocOtherHolderDebit = 'DOC_OTHER_HOLDER_DEBIT', // Envio DOC outra titularidade
@@ -33,11 +27,6 @@ export enum Value {
   TED_OTHER_HOLDER_CREDIT = 505,
   TED_SAME_HOLDER_REFUND_CREDIT = 1004,
   TED_OTHER_HOLDER_REFUND_CREDIT = 1005,
-
-  TEF_SAME_HOLDER_DEBIT = 1014,
-  TEF_OTHER_HOLDER_DEBIT = 1015,
-  TEF_SAME_HOLDER_CREDIT = 1024,
-  TEF_OTHER_HOLDER_CREDIT = 1025,
 
   DOC_SAME_HOLDER_DEBIT = 2,
   DOC_OTHER_HOLDER_DEBIT = 3,
